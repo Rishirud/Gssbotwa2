f1b147c8375acff3a8c928019c1ee47b4eb54b0c
Rishirudra <167420216+Rishirud@users.noreply.github.com>
2024-04-20 12:34:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "Arthmehta;
+global.ownername = "Arth mehta;
 global.owner = ['917988502368'];
 global.premium = ['917988502368'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
